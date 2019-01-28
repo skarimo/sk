96682e33e8bb5a8a69b468408920971b774e92be
skarimo <karimovj@gmail.com>
2019-01-28 12:06:01 -0500

    addes skills barsa

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import SkillBar from 'react-skillbars';
+import '../css/About.css';
 
-const skills = [
+const technicalSkills = [
   {type: "React", level: 85},
   {type: "Redux", level: 85},
   {type: "Javascript", level: 75},
@@ -9,6 +10,13 @@ const skills = [
   {type: "Rails", level: 75},
   {type: "CSS", level: 75},
   {type: "HTML", level: 75},
+]
+const softSkills = [
+  {type: "Leadership", level: 85},
+  {type: "Communication", level: 85},
+  {type: "Teamwork", level: 85},
+  {type: "Adaptability", level: 85},
+  {type: "Work Ethic", level: 85},
 ]
 
 const colors = {
@@ -23,20 +31,20 @@ const About = () => {
     return (
       <div id="about" className="body about">
         <h1 className="body-header highlight-primary left">about me</h1>
-          <div style={{width: '50%', float: 'left', paddingRight:'1%'}}>
-            <SkillBar skills={skills} width={'100px'} height={'20px'} colors={colors}/>
+
+
+          <div id='skills-container'>
+            <h3 className={'highlight-primary'} style={{fontSize:'150%'}}>Technical Skills</h3>
+            <SkillBar animationDelay={0} skills={technicalSkills} width={'100px'} height={'20px'} colors={colors}/>
+            <h3 className={'highlight-primary'}>Soft Skills</h3>
+            <SkillBar animationDelay={0} skills={softSkills} width={'100px'} height={'20px'} colors={colors}/>
           </div>
 
-        <p> lorem Ita tamen proident, export officia ut appellat. Eu incididunt sed expetendis ex
-        ea legam probant laboris, aliquip labore quem deserunt sint ab nam in veniam
-        sunt aute, pariatur cillum sunt pariatur tamen o an elit laboris et laborum ne
-        pariatur, se velit quid nisi eiusmod. Deserunt legam quae mentitum quorum.Dolor
-        o fabulas ea noster, nisi ita ut fugiat eiusmod ita tamen iis doctrina ut
-        multos, hic cillum ea multos. Possumus quo sint arbitror, ne quem anim nam
-        singulis. Aliquip quid de litteris concursionibus. Nulla consectetur doctrina
-        sunt aliquip, nescius firmissimum est consequat te culpa possumus excepteur,
-        expetendis o nescius est excepteur culpa arbitror ubi veniam ullamco voluptate
-        ne do fore fidelissimae an laboris noster ullamco.  </p>
+
+          <span style={{  fontStyle: 'italic'}}>'A journey of a thousand miles begins with a single step'</span> - Lao Tzu
+          <p>
+            My journey as a programmer lorem
+          </p>
       </div>
     )
   }
