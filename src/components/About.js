e1c3ebdecbc01a6ffeaa025a144264bce04a0c44
skarimo <karimovj@gmail.com>
2019-01-28 09:19:39 -0500

    added skills bars

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import SkillBar from 'react-skillbars';
 
-const About = (props) => {
+const skills = [
+  {type: "React", level: 85},
+  {type: "Redux", level: 85},
+  {type: "Javascript", level: 75},
+  {type: "Ruby", level: 75},
+  {type: "Rails", level: 75},
+  {type: "CSS", level: 75},
+  {type: "HTML", level: 75},
+]
+
+const colors = {
+  "bar": "#4bcffa",
+  "title": {
+    "text": "#fff",
+    "background": "#2980b9"
+  }
+}
+
+const About = () => {
     return (
       <div id="about" className="body about">
         <h1 className="body-header highlight-primary left">about me</h1>
+          <div style={{width: '50%', float: 'left', paddingRight:'1%'}}>
+            <SkillBar skills={skills} width={'100px'} height={'20px'} colors={colors}/>
+          </div>
 
         <p> lorem Ita tamen proident, export officia ut appellat. Eu incididunt sed expetendis ex
         ea legam probant laboris, aliquip labore quem deserunt sint ab nam in veniam
