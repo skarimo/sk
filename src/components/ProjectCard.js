bafc968bd11df962549db7c44e2a1a64f3289663
skarimo <karimovj@gmail.com>
2019-02-08 17:51:17 -0500

    fixes and added better project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,19 +3,9 @@ import React from 'react'
 const ProjectCard = ({ project }) => {
     return (
       <div className="project-cards" style={{
-          width: '300px',
-          height: '400px',
-          borderStyle: 'solid',
-          borderWidth: '1px',
-          borderRadius: '10px',
-          borderColor: 'white',
-          margin: '2%',
-          padding: '5px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          display: 'flex'
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${project.image})`
          }}>
-        <h3 className='highlight-secondary' style={{fontSize: '200%'}}>{ project.name }</h3>
+        <h3 className='highlight-secondary project-name' style={{fontSize: '200%'}}>{ project.name }</h3>
         <button className="project-view-button">View Project</button>
       </div>
     )
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,16 +7,22 @@ export const ProjectList = [
       name: "Stock Trainer",
       description: `Stock trainer is a realistic stock market trading game that makes use of real time stock
       data to allow users to buy and sell with in-game currency. Algorithm are employed to simulate real trading`,
-      image: stockTrainerImage
+      image: stockTrainerImage,
+      githubFront: 'https://github.com/skarimo/stock-trainer-frontend',
+      githubBack: 'https://github.com/skarimo/stock-trainer-backend'
     },
     {
       name: "Expense Logger",
       description: `A web application designed to keep log of users expense history as well as keep track money exchanges between users.`,
-      image: expenseLoggerImage
+      image: expenseLoggerImage,
+      githubFront: 'https://github.com/skarimo/expense-logger-frontend',
+      githubBack: 'https://github.com/skarimo/expense-logger-backend'
     },
     {
       name: "Face Tracker",
       description: `Allows user to keep track of their emotions and skin health through use of facial analysis.`,
-      image: faceTrackerImage
+      image: faceTrackerImage,
+      githubFront: 'https://github.com/skarimo/dft-frontend',
+      githubBack: 'https://github.com/skarimo/dft-backend'
     }
   ]
