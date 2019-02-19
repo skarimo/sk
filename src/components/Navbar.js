a29f184521a167cd4201607545fd7e2c89180fb7
skarimo <karimovj@gmail.com>
2019-02-18 21:34:35 -0500

    fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,23 +23,28 @@ class Navbar extends Component {
 
   render() {
       return (
-        <Menu right
-          isOpen={this.state.menuOpen}
-          onStateChange={(state) => this.handleStateChange(state)}
-        >
-          <Scrollchor to="#welcome" className="menu-item">
-            <span onClick={() => this.closeMenu()}>Main Page</span>
-          </Scrollchor>
-          <Scrollchor to="#about" className="menu-item">
-            <span onClick={() => this.closeMenu()}>About</span>
-          </Scrollchor>
-          <Scrollchor to="#projects" className="nav-links">
-            <span onClick={() => this.closeMenu()}>Projects</span>
-          </Scrollchor>
-          <Scrollchor to="#contact" className="nav-links">
-            <span onClick={() => this.closeMenu()}>Contact</span>
-          </Scrollchor>
-        </Menu>
+        <span onClick={() => this.closeMenu()}>
+          <Menu right
+            isOpen={this.state.menuOpen}
+            onStateChange={(state) => this.handleStateChange(state)}
+          >
+            <Scrollchor to="#welcome" className="menu-item">
+              Main Page
+            </Scrollchor>
+
+            <Scrollchor to="#about" className="menu-item">
+              About
+            </Scrollchor>
+
+            <Scrollchor to="#projects" className="nav-links">
+              Projects
+            </Scrollchor>
+
+            <Scrollchor to="#contact" className="nav-links">
+              Contact
+            </Scrollchor>
+          </Menu>
+      </span>
         )
      }
   }
