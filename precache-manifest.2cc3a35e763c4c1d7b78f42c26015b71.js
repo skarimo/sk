35cdb8e90238ab8b31f5609c85c5a2d5296fb083
skarimo <karimovj@gmail.com>
2019-02-19 16:09:59 -0500

    Updates

---
+++ precache-manifest.2cc3a35e763c4c1d7b78f42c26015b71.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "db66d14aec5150a8442c",
+    "url": "/sk/static/css/main.df789e39.chunk.css"
+  },
+  {
+    "revision": "db66d14aec5150a8442c",
+    "url": "/sk/static/js/main.db66d14a.chunk.js"
+  },
+  {
+    "revision": "58295e49ba1d881871ae",
+    "url": "/sk/static/js/1.58295e49.chunk.js"
+  },
+  {
+    "revision": "e883454c5ee3626176a0",
+    "url": "/sk/static/js/runtime~main.e883454c.js"
+  },
+  {
+    "revision": "c1e15984553469eccb95707b207f8771",
+    "url": "/sk/static/media/stock-trainer.c1e15984.png"
+  },
+  {
+    "revision": "691c6756fdcdfe533180e2a70a3c1a83",
+    "url": "/sk/static/media/face-tracker.691c6756.png"
+  },
+  {
+    "revision": "404727c111124ae5b786541a2df9135b",
+    "url": "/sk/static/media/expense-logger.404727c1.png"
+  },
+  {
+    "revision": "0e7b3cf95550aae463a2a5fcc4417b26",
+    "url": "/sk/static/media/headshot.0e7b3cf9.png"
+  },
+  {
+    "revision": "b8d900f06a62de6a82a41e306fc0931e",
+    "url": "/sk/static/media/background2.b8d900f0.jpg"
+  },
+  {
+    "revision": "025c74793f2ae17911b48566aa28106e",
+    "url": "/sk/index.html"
+  }
+];
